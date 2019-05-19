a72bfd45ed98c0b172609eaba3836f9b89784abd
charliechauri <carlos.echauri@gmail.com>
2019-05-19 16:45:31 -0500

    feat: Add deadline for initialization

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import './Styles/colors.css';
 import './Styles/sizes.css';
 
 function App() {
+  const currentDate = new Date();
+  const deadline = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    currentDate.getDate() + 1,
+    currentDate.getHours() + 23,
+    currentDate.getMinutes() + 27,
+    currentDate.getSeconds() + 47,
+  );
+
   return (
     <div className="App">
-      <CountdownTimer />
+      <CountdownTimer deadline={deadline} />
     </div>
   );
 }
