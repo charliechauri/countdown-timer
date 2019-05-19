852b596efa0c14e16c93c27ee8a433c3805e46d3
charliechauri <carlos.echauri@gmail.com>
2019-05-19 03:52:09 -0500

    feat: Add global styles and static CountdownTimer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Countdown } from './Components/CommonUI';
 import './App.css';
+import './Styles/colors.css';
+import './Styles/sizes.css';
 
 function App() {
   return (
     <div className="App">
-      <section className="countdown-timer" />
+      <section className="countdown-timer">
+        <Countdown indicator={1} label="Days" />
+
+        <Countdown indicator={23} label="Hours" />
+
+        <Countdown indicator={27} label="Minutes" />
+
+        <Countdown indicator={47} label="Seconds" />
+      </section>
     </div>
   );
 }
